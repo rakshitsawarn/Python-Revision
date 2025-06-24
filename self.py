c46139c416c6458f06460f41a9947c1c607c7cab
rakshitsawarn <rakshitsawarn@gmail.com>
2025-06-24 17:00:51 +0530

    in self I updated the code of calculator

---
+++ self.py
@@ -0,0 +1,35 @@
+a = int(input("Enter Number first: "))
+b = int(input("Enter Number second: "))
+
+class Calculator:
+    def add(self,a,b):
+        return a+b
+    def substraction(self,a,b):
+        return a-b
+    def multiplication(self,a,b):
+        return a*b
+    def division(self,a,b):
+        if b==0:
+            print("Error please change the value of b")
+        return a/b
+    def division_remove_float(self, a,b):
+        if b == 0:
+            print ("Change the value of b")
+        return a//b
+    def exponential(self, a, b):
+        return a ** b
+    
+        
+d = input("Enter the Desired Operation: ")
+
+if d == "Addition":
+    print(a+b)
+
+elif(d == "Substraction"):
+    print(a-b)
+
+elif(d == "Multiplication"):
+    print(a*b)
+
+elif(d == "Division"):
+    print (a/b)
---
+++ user_input.py
@@ -0,0 +1,8 @@
+a = input()
+
+a1 = int(input("Enter a Number: "))
+
+a2 = int(input())
+
+a3 = input("Enter your Name: ")
+
