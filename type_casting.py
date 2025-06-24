20fdd47f4773548f6fc2bbff031f6353aa2782c1
rakshitsawarn <rakshitsawarn@gmail.com>
2025-06-24 15:47:57 +0530

    typecasting

---
+++ type_casting.py
@@ -0,0 +1,14 @@
+#The Conversion of one datatype to another is type casting or type conversion.
+
+a = "1"
+b = "2"
+print(a + b) 
+
+print("Let's Convert a and b string to integer by function int(): ", int(a) + int(b))
+
+a = 8.9
+b = 3
+print("It is implicit typecasting",a+b)
+print(type(a))
+print(type(b))
+print(type(a+b))
