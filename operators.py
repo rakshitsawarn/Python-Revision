5cb0632176aba81cdc0db2dd0d2b27aff4ca81aa
rakshitsawarn <rakshitsawarn@gmail.com>
2025-06-24 14:54:25 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,23 @@
+a = 5 
+b = 7
+print (a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)  
+print(a//b)  
+print(a % b) 
+# Comparison operators
+print(a == b)  
+print(a != b) 
+print(a > b)   
+print(a < b)   
+print(a >= b)  
+print(a <= b)  
+# Logical operators
+print(a > 0 and b > 0)  
+print(a > 0 or b < 0)   
+print(not (a > 0))      
+# Identity operators
+print(a is b)
+print(a is not b)  
