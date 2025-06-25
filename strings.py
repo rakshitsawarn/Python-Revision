884c73fde393276c9663a295059d7fc59a59e981
rakshitsawarn <rakshitsawarn@gmail.com>
2025-06-25 12:34:10 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,11 @@
+print ("Hello")
+
+st = '''This is a multi-line string.It can span multiple lines
+and include special characters like \n and \t.'''
+
+print(st)
+
+name = "Rakshit Sawarn"
+
+for characters in name:
+    print(characters)
