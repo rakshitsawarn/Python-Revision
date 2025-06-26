f43958d7734aebcfae6cb31f9edabb5df06bbf12
rakshitsawarn <rakshitsawarn@gmail.com>
2025-06-26 15:59:28 +0530

    string methods

---
+++ string_methods.py
@@ -0,0 +1,10 @@
+# Strings are Immutable
+a = "Rakshit Sawarn!!!!!!!!!!!!!!!!!!!!!!!!"
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.split("!"))
+print(a.center(50))
+print(a.count("R"))
+
