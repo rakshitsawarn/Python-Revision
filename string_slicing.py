589cd34471745a6bc0210436804cd3cb5987c146
rakshitsawarn <rakshitsawarn@gmail.com>
2025-06-26 15:19:39 +0530

    slicing

---
+++ string_slicing.py
@@ -0,0 +1,11 @@
+names = "Rakshit Sawarn"
+print(names[0:5:2])
+print(len(names))
+print(names[:])
+print(names[:5])
+print(names[-3:])
+
+# Quick Quiz
+
+nm = "Harry"
+print("The OOutput is: ",nm[-4:-2])
