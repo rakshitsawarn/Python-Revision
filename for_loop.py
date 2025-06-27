f9c744af6359ca77d46f5e5d139b852db28f2986
rakshitsawarn <rakshitsawarn@gmail.com>
2025-06-27 16:37:45 +0530

    for - loop basic

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+colour = ['Red', 'Green', 'Blue', 'Yellow', 'Purple', 'Orange']
+for color in colour:
+    print(color)
+    for i in color:
+        print(i)
+
+
+name = "Rakshit Sawarn"
+for i in name:
+    print(i, end=',')
+
+for i in range(1,11):
+    print(i)
+
+for i in range(1,100,2):
+    print(i+1)
